f3955f27ca8033d5ba09f3317fb8123b61ec9898
sushant-suse <sushant.gaurav@suse.com>
2025-07-31 23:10:36 +0530

    reverted changes of test_contextmgr file
    Signed-off-by: sushant-suse <sushant.gaurav@suse.com>

--- tests/utils/test_contextmgr.py
+++ tests/utils/test_contextmgr.py
@@ -26,14 +26,31 @@ def test_timer_as_context_manager_measures_time():
     timer_name = 'test-timer'
     sleep_duration = 0.1
     timer = make_timer(timer_name)
-	@@ -36,6 +22,7 @@ def test_timer_as_context_manager_measures_time():
+
+    with timer() as timer_data:
+        time.sleep(sleep_duration)
+
+    elapsed = timer_data.elapsed
+    assert isinstance(elapsed, float)
+    assert elapsed == pytest.approx(sleep_duration, abs=0.05)
 
 
 def test_timer_factory_creates_independent_timers():
     timer_name1 = 'test-timer-1'
     timer_name2 = 'test-timer-2'
     sleep_duration = 0.1
-	@@ -54,124 +41,22 @@ def test_timer_factory_creates_independent_timers():
+
+    timer1 = make_timer(timer_name1)
+    timer2 = make_timer(timer_name2)
+
+    with timer1() as timer_data1:
+        time.sleep(sleep_duration)
+
+    with timer2() as timer_data2:
+        time.sleep(sleep_duration * 2)
+
+    assert timer_data1.name == timer_name1
+    assert timer_data2.name == timer_name2
     assert timer_data1.elapsed != timer_data2.elapsed
     assert isinstance(timer_data1.elapsed, float)
     assert isinstance(timer_data2.elapsed, float)
