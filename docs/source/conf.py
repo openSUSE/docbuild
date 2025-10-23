70ef636854fe2c8fea4f975feec0dd17b08c3085
sushant-suse <sushant.gaurav@suse.com>
2025-10-23 12:36:18 +0530

    refactor: removed old logo and added favicon.ico
    Signed-off-by: sushant-suse <sushant.gaurav@suse.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -188,7 +188,7 @@ html_theme_options = {
 
 html_logo = '_static/logo.png'
 
-html_favicon = '_static/logo.png'
+html_favicon = '_static/favicon.ico'
 
 # -- Options for intersphinx extension ---------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
