a6c776f253478561a489ab6a4f04192a59bac705
tomschr <tomschr@users.noreply.github.com>
2026-08-18 14:05:36 +0000

    deploy: ede81acfd64762b1ca5a2f8f9035758769d4454c

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.22.1.dev19+g3eae364ef.d20260818',
+    VERSION: '0.22.1.dev20+gede81acfd.d20260818',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
