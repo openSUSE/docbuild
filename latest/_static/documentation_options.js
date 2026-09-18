0eac3ca7541c166bce47500049718f847ab8b2f9
tomschr <tomschr@users.noreply.github.com>
2026-09-18 14:11:53 +0000

    deploy: fda9168d12a9bc1a5d3d04916bc9f69299468daa

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.23.1.dev36+g12e71b6a3.d20260918',
+    VERSION: '0.23.1.dev37+gfda9168d1.d20260918',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
